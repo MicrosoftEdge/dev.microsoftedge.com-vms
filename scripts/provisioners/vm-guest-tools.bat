0bc5d2bc77354c6c29fa50f4380361bd26a031b0
Eduard Tomas <etomas@gmail.com>
2017-06-23 13:07:15 +0200

    Update scripts for register Oracle certificates using vbbox-tools CD, instead relying in a cer copied in floppy

--- scripts/provisioners/vm-guest-tools.bat
+++ scripts/provisioners/vm-guest-tools.bat
@@ -62,7 +62,9 @@ goto :done
 
 :: There needs to be Oracle CA (Certificate Authority) certificates installed in order
 :: to prevent user intervention popups which will undermine a silent installation.
-cmd /c certutil -addstore -f "TrustedPublisher" A:\oracle-cert.cer
+cmd /c "e: && cd cert && for %%i in (vbox*.cer) do VBoxCertUtil add-trusted-publisher %%i --root %%i"
+
+:: certutil -addstore -f "TrustedPublisher" A:\oracle-cert.cer
 
 ::move /Y C:\Users\IEUser\VBoxGuestAdditions.iso C:\Windows\Temp
 ::cmd /c ""C:\Program Files\7-Zip\7z.exe" x C:\Windows\Temp\VBoxGuestAdditions.iso -oC:\Windows\Temp\virtualbox"
